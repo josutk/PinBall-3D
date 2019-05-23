d3fa0e0e30fba97e5d9f3c8b54e5c53c09b585c8
Roger Lenke <rogerlenke@gmail.com>
2019-05-23 10:21:03 -0300

    Creating Spinner Model

---
+++ Pinball/Assets/Scripts/SpinnerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerScript : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public int turns
+    { get; set; }
+
+    // Component score
+    private const int SCORE = 100;
+
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(rb.angularVelocity != Vector3.zero)
+        {
+            Debug.Log("Moving!");
+
+            if(transform.rotation.x > 180 || transform.rotation.x < 1)
+            {
+                Debug.Log("Rotated!");
+            }
+        }
+    }
+}
