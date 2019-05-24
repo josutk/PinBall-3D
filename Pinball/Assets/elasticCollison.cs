410e59b16465f1eb9a05d60d22d8f97a395eb18d
josutk <josuetk63@gmail.com>
2019-05-23 22:43:32 -0300

    Apply increase velocity after collide with slingshot

---
+++ Pinball/Assets/elasticCollison.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elasticCollison : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int increaseVelocity = 15;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (sphereColision(collision)) {
+
+            Vector3 vector = collision.rigidbody.velocity;
+            vector.x = (collision.transform.position.x - transform.position.x) * increaseVelocity;
+            vector.y = (collision.transform.position.y - transform.position.y) * increaseVelocity;
+            vector.z = (collision.transform.position.z - transform.position.z) * increaseVelocity;
+            collision.rigidbody.velocity = vector;
+        }
+    }
+
+    private bool sphereColision( Collision collision) {
+
+        if (collision.gameObject.name == "Sphere")
+        {
+            return true;
+        }
+        return false;
+    } 
+
+}
