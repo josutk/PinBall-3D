dd02e98c684f0b1341df698d845c7e09a71dd2fd
josutk <josuetk63@gmail.com>
2019-05-24 00:35:19 -0300

    Apply elastic collison to pop-bumper

--- Pinball/Assets/Scripts/elasticCollison.cs
+++ Pinball/Assets/Scripts/elasticCollison.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class elasticCollison : MonoBehaviour
+public class ElasticCollison : MonoBehaviour
 {
     // Start is called before the first frame update
     private int increaseVelocity = 15;
