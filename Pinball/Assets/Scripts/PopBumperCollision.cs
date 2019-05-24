5e71694a1446df1c6b20c61e1677341e11ff5bdc
josutk <josuetk63@gmail.com>
2019-05-24 01:11:57 -0300

    Change color when hit the body

--- Pinball/Assets/Scripts/PopBumperCollision.cs
+++ Pinball/Assets/Scripts/PopBumperCollision.cs
@@ -2,40 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElasticCollison : MonoBehaviour
+public class PopBumperCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private ElasticCollison elastic = new ElasticCollison();
     private int increaseVelocity = 15;
+    public Color color = Color.black;
+
+    // Start is called before the first frame update
     void Start()
     {
+        gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, color.a);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       
         
     }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (sphereColision(collision)) {
+    void OnCollisionEnter(Collision collision) {
 
+        if (elastic.sphereColision(collision)) {
+
+            gameObject.GetComponent<Renderer>().material.color = changeColor();
             Vector3 vector = collision.rigidbody.velocity;
             vector.x = (collision.transform.position.x - transform.position.x) * increaseVelocity;
             vector.y = (collision.transform.position.y - transform.position.y) * increaseVelocity;
             vector.z = (collision.transform.position.z - transform.position.z) * increaseVelocity;
             collision.rigidbody.velocity = vector;
+
+        
         }
     }
 
-    public bool sphereColision( Collision collision) {
+    private Color changeColor() {
 
-        if (collision.gameObject.name == "Sphere")
-        {
-            return true;
-        }
-        return false;
-    } 
+        Color newColor = Color.green;
+        return new Color(newColor.r, newColor.g, newColor.b, newColor.a);
 
+    }
 }
