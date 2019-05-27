0b085deda69eaee65e837f888abf2fac4d97616f
Roger Lenke <rogerlenke@gmail.com>
2019-05-27 12:36:57 -0300

    Creating scoop.

---
+++ Pinball/Assets/Scripts/Constants.cs
@@ -0,0 +1,6 @@
+
+public static class Constants
+{
+    public const string SPHERE_TAG = "Sphere";
+    public const string TABLE_TAG = "Table";
+}
---
+++ Pinball/Assets/Scripts/ScoopScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoopScript : MonoBehaviour
+{
+    private GameObject sphere;
+    private GameObject[] table;
+
+    void Start()
+    {
+        sphere = GameObject.FindGameObjectWithTag(Constants.SPHERE_TAG);
+        table = GameObject.FindGameObjectsWithTag(Constants.TABLE_TAG);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(DidCollideWithSphere(other))
+        {
+            foreach(GameObject t in table)
+            {
+                Physics.IgnoreCollision(other, t.GetComponent<Collider>());
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(DidCollideWithSphere(other))
+        {
+            foreach(GameObject t in table)
+            {
+                Physics.IgnoreCollision(other, t.GetComponent<Collider>(), false);
+            }
+        }
+    }
+
+    bool DidCollideWithSphere(Collider other)
+    {
+        if(other.gameObject.tag == Constants.SPHERE_TAG)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
