28aebf4842542f70b7993f227d67cd96d38efef5
josutk <josuetk63@gmail.com>
2019-05-27 17:44:40 -0300

    Update Slingshot

--- Pinball/Assets/Scripts/rigidCollison.cs
+++ Pinball/Assets/Scripts/rigidCollison.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rigidCollison : MonoBehaviour
+public class RigidCollison : MonoBehaviour
 {
     // Start is called before the first frame update
     private float increaseVelocity = 0.5f;
