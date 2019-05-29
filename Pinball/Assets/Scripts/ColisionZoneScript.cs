0c3bc05262d39824aa10c86fdff11e909eeabfb4
Igor Gabriel <igorgabriel515@gmail.com>
2019-05-28 22:34:28 -0300

    Fixing gameover.

---
+++ Pinball/Assets/Scripts/ColisionZoneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionZoneScript : MonoBehaviour
+{
+    public Transform spawnPosition;
+    public GameObject sphere;
+    public Vector3 initPosition;
+
+    void Start() {
+        Instantiate(sphere, spawnPosition.position, sphere.transform.rotation);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+       if(!GameObject.FindGameObjectWithTag("Sphere")) {
+            collision.rigidbody.velocity = Vector3.zero;
+            collision.transform.position = initPosition;
+        }
+    }
+}
