509e6e0b15fa511bad1698830c275afe9b0adb71
Igor Gabriel <igorgabriel515@gmail.com>
2019-06-02 00:49:37 -0300

    Applying game over condition.

--- Pinball/Assets/Scripts/ColisionZoneScript.cs
+++ Pinball/Assets/Scripts/ColisionZoneScript.cs
@@ -32,62 +32,10 @@ public class ColisionZoneScript : MonoBehaviour {
                 Destroy(GameObject.FindGameObjectWithTag("Sphere"));
             }
             if (Input.GetKeyDown(KeyCode.Space)) {
-                SceneManager.LoadScene("BoardScene");
+                SceneManager.LoadScene("FGArcadeScene");
                 
             }
         }
                 
     }
-
-    //void Start() {
-    //    Instantiate(sphere, spawnPosition.position, sphere.transform.rotation);
-    //    //initPosition = spawnPosition.position;
-    //}
-
-    //void OnTriggerEnter(Collider other) {
-    //    if (lives < 0) {
-    //        gameOver = true;
-    //    }
-    //    if (other.gameObject.tag == "Sphere" && !gameOver) {
-    //        lives--;
-    //        Destroy(sphere);
-    //        Instantiate(sphere, spawnPosition.position, sphere.transform.rotation);
-    //        //other.GetComponent<Rigidbody>().velocity = Vector3.zero;           
-    //        //other.transform.position = initPosition;
-    //        LivesMangerScript.livesM = lives;
-    //    }
-
-    //}
-
-    //void OnTriggerEnter(Collider other) {
-    //    if (lives < 0) {
-    //        gameOver = true;
-    //    }
-    //    if (other.gameObject.tag == "Sphere" && !gameOver) {
-    //        Destroy(sphere);
-    //        //other.GetComponent<Rigidbody>().velocity = Vector3.zero;
-    //        lives--;
-    //        Instantiate(sphere, spawnPosition.position, sphere.transform.rotation);
-    //        //other.transform.position = initPosition;
-    //    }
-
-    //}
-
-    // void Update() {
-    //if (lives < 0) {
-    //    gameOver = true;
-    //}
-    //if (!GameObject.FindGameObjectWithTag("Sphere") && !gameOver) {
-    //    lives--;
-    //    Instantiate(sphere, spawnPosition.position, sphere.transform.rotation);
-
-    //}
-    //}
-
-    //void OnCollisionEnter(Collision collision) {
-    //   if(!GameObject.FindGameObjectWithTag("Sphere")) {
-    //        collision.rigidbody.velocity = Vector3.zero;
-    //        collision.transform.position = initPosition;
-    //    }
-    //}
 }
