4b170d002894c5babbb05a2c80623ca073646889
Roger Lenke <rogerlenke@gmail.com>
2019-06-03 17:20:03 -0300

    Creating more walls.

---
+++ Pinball/Assets/Scripts/DisableWallTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableWallTrigger : MonoBehaviour
+{
+    public MoveUpBehaviour wallTrigger;
+
+    void OnTriggerExit(Collider collider)
+    {
+        wallTrigger.enabled = false;
+    }
+}
