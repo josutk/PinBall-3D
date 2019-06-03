f7ddc2234444b3002d5e0598b71997df99772489
Igor Gabriel <igorgabriel515@gmail.com>
2019-06-03 00:34:07 -0300

    Applying partially behavior to slingshots models.

--- Pinball/Assets/Scripts/ElasticCollision.cs
+++ Pinball/Assets/Scripts/ElasticCollision.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PopBumperCollision : MonoBehaviour
+public class ElasticCollision : MonoBehaviour
 {
     //private ElasticCollison elastic = new ElasticCollison();
-    private int increaseVelocity = 15;
+    public int increaseVelocity = 15;
+    public Color startColor, changeColor;
 
     // Start is called before the first frame update
     void Start(){        
@@ -17,9 +18,8 @@ public class PopBumperCollision : MonoBehaviour
 
     void OnCollisionEnter(Collision collision) {
 
-        if (sphereColision(collision)) {
-
-            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        if (sphereColision(collision)) {            
+            gameObject.GetComponent<Renderer>().material.color = changeColor;
             Vector3 vector = collision.rigidbody.velocity;
             vector.x = (collision.transform.position.x - transform.position.x) * increaseVelocity;
             vector.y = (collision.transform.position.y - transform.position.y) * increaseVelocity;
@@ -30,9 +30,9 @@ public class PopBumperCollision : MonoBehaviour
         }
     }
 
-    void OnCollisionExit(Collision other){
+    void OnCollisionExit(Collision other){        
         if (sphereColision(other)) {
-            gameObject.GetComponent<Renderer>().material.color = Color.gray;
+            gameObject.GetComponent<Renderer>().material.color = startColor;
         }
     }
 
