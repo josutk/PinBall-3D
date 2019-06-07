c08e522f1e59b0d4863e2dfa97dd506cbe0624fa
Roger Lenke <rogerlenke@gmail.com>
2019-06-06 23:03:06 -0300

    Adding some audio cues.

--- Pinball/Assets/Scripts/Behaviours/SoundBehaviour.cs
+++ Pinball/Assets/Scripts/Behaviours/SoundBehaviour.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class SoundBehaviour : MonoBehaviour
 {
+    // Audio source that will play with collision with the ball.
+    // If no audio source is passed, it tries to get one from the
+    // Current object.
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        if(audioSource == null) audioSource = GetComponent<AudioSource>();
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if(CollisionHelper.DidCollideWithSphere(collision.gameObject.tag))
         {
-            AudioSource source = GetComponent<AudioSource>();
-            source.Play();
+            audioSource.Play();
         }
     }
 }
