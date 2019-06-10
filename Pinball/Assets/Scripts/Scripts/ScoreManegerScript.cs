801031140c914ddf20b6afdb5fa92b342c5e86e3
Igor Gabriel <igorgabriel515@gmail.com>
2019-06-10 00:37:45 -0300

    Fixing Score manager.

--- Pinball/Assets/Scripts/Scripts/ScoreManegerScript.cs
+++ Pinball/Assets/Scripts/Scripts/ScoreManegerScript.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreManegerScript : MonoBehaviour {
-    
+
     public int score { get; private set; }
 
-    public TextMesh scoreText;
+    public TextMesh scoreText;    
     private string scoreTextFormat = "Score: {0}";
-
-    //public TextMesh scoreEndText;
-    //public string scoreEndTextFormat = "Final Score: {0}";
 
     void Start() {
         AddScore(0);
@@ -21,13 +18,8 @@ public class ScoreManegerScript : MonoBehaviour {
         UpdateScore();
     }
 
-    //public void ClearScore() {
-    //    score = 0;
-    //    UpdateScore();
-    //}
-
     void UpdateScore() {
-        scoreText.text = string.Format(scoreTextFormat, this.score);
-        // scoreEndText.text = string.Format(scoreEndTextFormat, this.score);
+        //scoreText.text = string.Format(scoreTextFormat, this.score);
+        scoreText.text = this.score.ToString().PadLeft(12, '0');
     }
 }
