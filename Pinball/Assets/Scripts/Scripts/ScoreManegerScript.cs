808fda06695cb9f8d2605376e81780bd1bacfe70
Igor Gabriel <igorgabriel515@gmail.com>
2019-06-09 22:40:06 -0300

    Creating score script.

---
+++ Pinball/Assets/Scripts/Scripts/ScoreManegerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManegerScript : MonoBehaviour {
+    
+    public int score { get; private set; }
+
+    public TextMesh scoreText;
+    private string scoreTextFormat = "Score: {0}";
+
+    //public TextMesh scoreEndText;
+    //public string scoreEndTextFormat = "Final Score: {0}";
+
+    void Start() {
+        AddScore(0);
+    }
+
+    public void AddScore(int score) {
+        this.score += score;
+        UpdateScore();
+    }
+
+    //public void ClearScore() {
+    //    score = 0;
+    //    UpdateScore();
+    //}
+
+    void UpdateScore() {
+        scoreText.text = string.Format(scoreTextFormat, this.score);
+        // scoreEndText.text = string.Format(scoreEndTextFormat, this.score);
+    }
+}
