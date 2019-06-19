7b224458211330b5fd741ffacfa1b4a9b2c818ae
josutk <josuetk63@gmail.com>
2019-06-19 17:40:57 -0300

    Create DropTarget2d

---
+++ Pinball/Assets/Scripts/DropTarget2DScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTarget2DScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    bool renderState;
+    void Start()
+    {
+        renderState = true;
+        GetComponent<Renderer>().enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (renderState == true)
+        {
+            GetComponent<Renderer>().enabled = false;
+            renderState = false;
+            Physics2D.IgnoreCollision(collider.gameObject.GetComponent<Collider2D>(),GetComponent<Collider2D>());
+        }else
+        {
+            GetComponent<Renderer>().enabled = true;
+            renderState = true;
+        }
+    }
+}
