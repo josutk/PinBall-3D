e824d2dbff363dd5a14ccc84f618ecc6e71df9e8
Allan Nobre <allanjefrey9@hotmail.com>
2019-07-10 03:18:52 -0300

    Enabling stop and next music

--- Pinball/Assets/Scripts/Behaviours/BackgroundMusicBehaviour.cs
+++ Pinball/Assets/Scripts/Behaviours/BackgroundMusicBehaviour.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class BackgroundMusicBehaviour : MonoBehaviour
 {
     public AudioSource musicSource;
-    public AudioClip musicClip;
-    public bool paused;
+    public AudioClip[] soundtrack;
+    public int currentMusic;
     private SignalHandlerScript signalHandler;
     private float elapsedTime;
+    private bool musicPassed;
 
     // Start is called before the first frame update
     void Start()
     {
         if (musicSource == null) musicSource = GetComponent<AudioSource>();
         signalHandler = Finder.GetSignalHandler();
-        musicSource.clip = musicClip;
+        musicSource.clip = soundtrack[currentMusic];
         musicSource.volume = 0.5F;
-        paused = false;
+        musicPassed = false;
         elapsedTime = 0F;
 
         musicSource.Play();
@@ -26,41 +27,60 @@ public class BackgroundMusicBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PauseMusicBehavior();
+        CountTimeSelectPressed();
+        StopMusic();
+        PlayNextMusic();
     }
 
-    public void PauseMusicBehavior()
+    void CountTimeSelectPressed()
     {
-        if (signalHandler.buttons.select && !paused)
+        if (signalHandler.buttons.select)
         {
             elapsedTime += Time.deltaTime;
-            if (elapsedTime >= 3.0)
-            {
-                musicSource.Pause();
-                elapsedTime = 0;
-                paused = true;
-            }
-        }
-        else if (signalHandler.buttons.select && paused)
-        {
-            elapsedTime += Time.deltaTime;
-            if (elapsedTime >= 3.0)
-            {
-                musicSource.UnPause();
-                paused = false;
-                elapsedTime = 0;
-            }
         }
         else
         {
+            musicPassed = false;
             elapsedTime = 0;
         }
     }
 
-    public void ChangeMusic(AudioClip nextMusicClip)
+    void StopMusic()
+    {
+        if (signalHandler.buttons.select && musicSource.isPlaying && elapsedTime >= 6.0)
+        {
+            musicSource.Stop();
+        }
+    }
+
+    void PlayNextMusic()
+    {
+        if (signalHandler.buttons.select && !musicPassed && elapsedTime >= 3.0)
+        {
+            ChangeMusic(soundtrack[GetValidNextMusic()]);
+            musicPassed = true;
+        }
+    }
+
+    void ChangeMusic(AudioClip nextMusicClip)
     {
         musicSource.Stop();
         musicSource.clip = nextMusicClip;
         musicSource.Play();
+    }
+
+    int GetValidNextMusic()
+    {
+        int nextMusic = currentMusic + 1;
+        if (nextMusic > soundtrack.Length)
+        {
+            nextMusic = 0;
+            currentMusic = nextMusic;
+            return nextMusic;
+        } else
+        {
+            currentMusic = nextMusic;
+            return nextMusic;
+        }
     }
 }
